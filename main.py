49da1079806889e6635c82ee4b452c190e84a354
thlurte <thlurte@gmail.com>
2023-06-29 12:50:24 +0530

    added hypothesis api to hub

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template,request
+from package.hub import Hub
+import requests
 
+
+## Flask initial config 
 app = Flask(__name__)
 
-class API_KEYS:
-    def __init__(self,HYPOTHESIS_KEY=None,HUGGING_FACE_KEY=None):
-        self.HYPOTHESIS_KEY = HYPOTHESIS_KEY
-        self.HUGGING_FACE_KEY = HUGGING_FACE_KEY 
 
-Keys = API_KEYS()
-
+Keys = Hub()
+## Route Setup
 @app.route("/")
 def index():
     return render_template('index.html',Keys=Keys)
@@ -16,28 +16,30 @@ def index():
 
 @app.route("/save",methods=['GET','POST'])
 def save():
-    if request.method == "POST":
-        Keys.HUGGING_FACE_KEY = request.form.get('hugg')
-        Keys.HYPOTHESIS_KEY = request.form.get('hyp')
+    if request.method=="POST":
+        Keys.HUGGINGFACE_API_KEY = request.form.get('hugg')
+        Keys.HYPOTHESIS_API_KEY = request.form.get('hyp')
         return render_template('index.html',Keys=Keys)
     else:
         return render_template('save.html',Keys=Keys)
 
 @app.route("/forget")
 def forget():
-    Keys.HUGGING_FACE_KEY = None
-    Keys.HYPOTHESIS_KEY = None
+    Keys.HUGGINGFACE_API_KEY = None
+    Keys.HYPOTHESIS_API_KEY = None
     return render_template('index.html',Keys=Keys)
 
 
 @app.route("/dashboard")
 def dashboard():
+    
     return render_template('dashboard.html',Keys=Keys)
 
 
 @app.route("/nodes")
 def nodes():
-    return render_template('nodes.html',Keys=Keys)
+    Nodes = Keys.node_dispatcher()
+    return render_template('nodes.html',Keys=Keys,Nodes=Nodes)
 
 @app.route("/tars")
 def tars():
---
+++ package/hub.py
@@ -0,0 +1,15 @@
+import requests
+class Hub:
+  def __init__(self,HYPOTHESIS_API_KEY=None,HUGGINGFACE_API_KEY=None,HYPOTHESIS_USERNAME="thlurte"):
+    self.HYPOTHESIS_USERNAME = HYPOTHESIS_USERNAME
+    self.HYPOTHESIS_API_KEY = HYPOTHESIS_API_KEY
+    self.HUGGINGFACE_API_KEY = HUGGINGFACE_API_KEY
+    
+  def node_dispatcher(self):
+    """
+    Function -> returns all nodes from hypothsis.is
+    """
+    headers = { "Authorization": f"Bearer {self.HYPOTHESIS_API_KEY}" }
+    para    = { "limit":50,"user": f"acct:{self.HYPOTHESIS_USERNAME}@hypothes.is" }
+    api_url = "https://hypothes.is/api/search"
+    return requests.get(api_url,headers=headers,params=para).json()['rows']
